00e7ddec0b32ec0b8ccf715331f07eeaece49ac0
mustafatok2016 <mustafa.tk33@gmail.com>
2020-08-25 16:17:21 +0000

    added rds instance

--- module/module.tf
+++ module/module.tf
@@ -1,42 +1,27 @@
-
-# Module to create RDS
-
-### This repo requires mysql client.
-### Please install by running 
-```
-sudo yum install mysql -y
-```
-
-## Create a file module.tf and add the following
-
-
-
-```
 module "db" {
-    source = "./class7/instance"
+    source = "terraform-aws-rds-instance"
     region = "us-east-2"
     subnet_ids = [
-    "subnet-0764b3812f98463f4", 
-    "subnet-01a630df412c0aab8", 
-    "subnet-07655cc9da49d8de5"
+    "subnet-01fd890eaff2139b7", 
+    "subnet-06e2b411c295d270d", 
+    "subnet-00f8f8482e906ba1f"
     ]
     security_group_name = "db"
     allowed_hosts = [
     "0.0.0.0/0"
     ]
     db_name = "dbname"
-    engine = "mysql"
-    engine_version = "5.7"
+    engine = "aurora"
+    engine_version = "5.6.10a"
     instance_class = "db.t2.micro"
     username = "foo"
     password = "foobarbaz"
     publicly_accessible = true
 }
 
-```
 
-## Create another file output.tf
-```
+
+
 output "region" {
 value = "${module.db.region}"
 }
@@ -49,10 +34,3 @@ value = "${module.db.allowed_hosts}"
 output "DB_NAME" {
 value = "${module.db.DB_NAME}"
 }
-```
-
-## Run 
-```
-terraform init 
-terraform apply
-```
